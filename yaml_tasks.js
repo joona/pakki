022c908db81b2c06304caba1086a3e804bf894aa
Joona Kulmala <jmkulmala@gmail.com>
2019-08-29 22:45:06 +0300

    renamed readPage(s) to readDocument and kept aliases

--- yaml_tasks.js
+++ yaml_tasks.js
@@ -5,7 +5,7 @@ const yaml = require('node-yaml');
 const { glob } = require('./utils');
 
 const tasks = module.exports = {
-  async readPage(ctx, file, relativePath) {
+  async readDocument(ctx, file, relativePath) {
     const content = await yaml.read(file);
     const fileKey = path.basename(path.relative(relativePath, file), path.extname(file));
 
@@ -17,18 +17,26 @@ const tasks = module.exports = {
     };
   },
 
-  async readPages(ctx, globPattern) {
+  async readDocuments(ctx, globPattern) {
     const { source } = ctx;
     const fullPath = path.join(source, globPattern);
     const relativePath = path.dirname(fullPath);
     const files = await glob(fullPath);
 
     const items = await bluebird.map(files, file => {
-      return tasks.readPage(ctx, file, relativePath);
+      return tasks.readDocument(ctx, file, relativePath);
     });
 
-    console.log('[readPages] processed', items.length, 'files');
+    console.log('[readDocuments] processed', items.length, 'files');
 
     return items;
+  },
+
+  readPages(ctx, globPattern) {
+    return tasks.readDocuments(ctx, globPattern);
+  },
+
+  readPage(ctx, file, relativePath) {
+    return tasks.readDocument(ctx, file, relativePath);
   }
 };
