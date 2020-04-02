be250fbfc8025fafa2bb208d9ab1b07a9522119e
Joona Kulmala <jmkulmala@gmail.com>
2020-04-03 00:40:38 +0300

    added multilingual sitemap support with proper alternate tags to alternative locales

--- sitemap_tasks.js
+++ sitemap_tasks.js
@@ -9,6 +9,8 @@ module.exports = {
 
     const urls = [];
     const urlSet = new Set();
+    const handledKeys = new Set();
+    const hasMultipleLocales = ctx.locales && Array.isArray(ctx.locales) && ctx.locales.length > 1;
 
     if(!sitemap) {
       sitemap = ctx.sitemap;
@@ -23,22 +25,47 @@ module.exports = {
       return;
     }
 
+    if(hasMultipleLocales && ctx.primaryLocale) {
+      sitemap = sitemap.sort((a, b) => {
+        if(b.locale == a.locale) return 0;
+        return b.locale == ctx.primaryLocale ? 1 : -1;
+      });
+    }
+
     //console.log('Sitemap:', site.sitemap);
     sitemap.forEach(item => {
       if(item.ignore) return;
       if(urlSet.has(item.url)) return;
+      if(handledKeys.has(item.key)) return;
+
       let prio = item.prio || (item.isAlias ? 0.7 : 1);
 
-      console.log('[buildSitemap] adding item ', item.url, prio, item.isAlias);
+      console.log('[buildSitemap] adding item ', item.key, item.url, prio, item.isAlias);
+
+      let links = [];
+      if(hasMultipleLocales && ctx.primaryLocale == item.locale && item.key) {
+        links = sitemap.filter(p => p.key == item.key)
+          .map(p => {
+            return {
+              lang: p.locale,
+              url: p.url
+            };
+          });
+      }
 
       urls.push({
         url: item.url,
         changefreq: item.frequency || 'weekly',
         priority: prio,
-        lastmodrealtime: item.updated_at || true
+        lastmodrealtime: item.updated_at || true,
+        links
       });
 
       urlSet.add(item.url);
+
+      if(item.key) {
+        handledKeys.add(item.key);
+      }
     });
 
     const sitemapContent = sitemapBuilder.createSitemap({
