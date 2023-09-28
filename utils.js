bad1874eb425fdd969e44aa499db644b980fee77
Joona Kulmala <joona@rootz.com>
2023-09-28 11:26:44 +0300

    remove mkdirp dependency and replace with custom implementation

--- utils.js
+++ utils.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const util = require('util');
 const path = require('path');
-const mkdirp = require('mkdirp');
 const recursiveCopy = require('recursive-copy');
 const crypto = require('crypto');
 
@@ -10,13 +9,47 @@ const writeFile = util.promisify(fs.writeFile);
 const readFile = util.promisify(fs.readFile);
 
 const utils = module.exports = {
-  mkdirp,
   writeFile,
   readFile,
   recursiveCopy,
 
+  async mkdirp(targetDir, options = { isRelativeToScript: false }) {
+    const sep = path.sep;
+    if (path.extname(targetDir)) {
+      targetDir = path.dirname(targetDir);
+    }
+
+    const initDir = path.isAbsolute(targetDir) ? sep : "";
+    const baseDir = options.isRelativeToScript ? __dirname : ".";
+
+    return targetDir.split(sep).reduce((parentDir, childDir) => {
+      const curDir = path.resolve(baseDir, parentDir, childDir);
+      try {
+        fs.mkdirSync(curDir);
+      } catch (err) {
+        if (err.code === "EEXIST") {
+          // curDir already exists!
+          return curDir;
+        }
+
+        // To avoid `EISDIR` error on Mac and `EACCES`-->`ENOENT` and `EPERM` on Windows.
+        if (err.code === "ENOENT") {
+          // Throw the original parentDir error on curDir `ENOENT` failure.
+          throw new Error(`EACCES: permission denied, mkdir '${parentDir}'`);
+        }
+
+        const caughtErr = ["EACCES", "EPERM", "EISDIR"].indexOf(err.code) > -1;
+        if (!caughtErr || (caughtErr && curDir === path.resolve(targetDir))) {
+          throw err; // Throw if it's just the last created dir.
+        }
+      }
+
+      return curDir;
+    }, initDir);
+  },
+
   async glob(patterns) {
-    patterns = Array.isArray(patterns) ? patterns : [ patterns ];
+    patterns = Array.isArray(patterns) ? patterns : [patterns];
 
     const files = await Promise.all(patterns.map(p => {
       return glob(p);
@@ -26,7 +59,7 @@ const utils = module.exports = {
   },
 
   copy(source, target) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
       var rd = fs.createReadStream(source);
       rd.on('error', rejectCleanup);
       var wr = fs.createWriteStream(target);
@@ -50,12 +83,12 @@ const utils = module.exports = {
 
   translateUrlToStaticPath(ctx, destPath) {
     const { dest } = ctx;
-    const url = destPath; 
-    if(destPath.charAt(0) == '/') destPath = destPath.substr(1);
+    const url = destPath;
+    if (destPath.charAt(0) == '/') destPath = destPath.substr(1);
     destPath = path.join(dest, destPath);
 
     const extension = path.extname(destPath);
-    if(!extension) {
+    if (!extension) {
       destPath = path.join(destPath, 'index.html');
     }
 
@@ -68,13 +101,13 @@ const utils = module.exports = {
   },
 
   async writeSiteFile(ctx, url, contentOrFactory) {
-    if(!url) {
+    if (!url) {
       console.log('contentFactory:', contentOrFactory);
       throw new Error('I dunno where to write the page, because url is not defined!');
     }
     const { basePath, filePath } = utils.translateUrlToStaticPath(ctx, url);
 
-    if(!filePath) {
+    if (!filePath) {
       console.log('failed to resolve path:', basePath, filePath, ' / ', url);
       throw new Error('I dunno where to write the page, because file path resolving failed!');
     }
@@ -82,16 +115,16 @@ const utils = module.exports = {
     let content = null;
 
     // allow returning the content from a factory
-    if(typeof contentOrFactory === 'function') {
+    if (typeof contentOrFactory === 'function') {
       content = await contentOrFactory(ctx, { filePath, basePath, url });
     } else {
       content = contentOrFactory;
     }
 
     try {
-      await mkdirp(basePath);
+      await this.mkdirp(basePath);
       await writeFile(filePath, content);
-    } catch(err) {
+    } catch (err) {
       console.error(`[writeSiteFile] error while writing ${filePath} (${url})`, err.message);
       console.error(err.stack);
       throw err;
@@ -101,7 +134,7 @@ const utils = module.exports = {
   },
 
   async fileHash(ctx, filePath, algorithm) {
-    if(!algorithm) {
+    if (!algorithm) {
       algorithm = ctx.hashAlgorithm || 'md5';
     }
 
